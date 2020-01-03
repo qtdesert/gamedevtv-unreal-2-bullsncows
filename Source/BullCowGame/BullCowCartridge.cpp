f1dade5dba0180a42daa7d902274afc9996a2f11
Quentin Désert <qtdesert@gmail.com>
2020-01-02 19:51:38 -0600

    Basic game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,21 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 6 letter word.")); //TODO: Magic number remove!
     PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("forest");
+    FString HiddenWord = TEXT("forest"); //TODO: Move outside this function
     PrintLine(Input);
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You loose... Try again!"));
+    }
 }
